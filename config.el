d8d107c1491fca8ef9070671380ecd7bd61106a8
Björn Erlwein <bjoern.erlwein@empiriecom.com>
2019-07-12 23:16:05 +0200

    initial commit

---
+++ config.el
@@ -0,0 +1,9 @@
+;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+
+(setq
+ projectile-project-search-path '("~/random programmier zeug/" "~/emma/")
+ ns-right-option-modifier 'none
+ ns-right-command-modifier 'none
+ )
