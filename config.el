ca6b63a08b5bbefd7046fea8b98846af59ef2b34
berlwein <bjoern.erlwein@empiriecom.com>
2020-05-10 23:04:41 +0200

    work config

--- config.el
+++ config.el
@@ -2,6 +2,59 @@
 
 ;; Place your private configuration here
 
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; refresh' after modifying this file!
+
+
+;; These are used for a number of things, particularly for GPG configuration,
+;; some email clients, file templates and snippets.
+(setq user-full-name "Björn Erlwein"
+      user-mail-address "bjoern.erlwein@empiriecom.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; test
+                                        ;(setq doom-font (font-spec :family "monospace" :size 14)
+                                        ;      doom-variable-pitch-font (font-spec :family "sans"))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. These are the defaults.
+(setq doom-theme 'doom-one)
+
+;; If you intend to use org, it is recommended you change this!
+(setq org-directory "~/org/")
+
+;; If you want to change the style of line numbers, change this to `relative' or
+;; `nil' to disable it:
+(setq display-line-numbers-type t)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', where Emacs
+;;   looks when you load packages with `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; they are implemented.
+
 (setq
  projectile-project-search-path '("~/random programmier zeug/" "~/emma/")
  ns-right-option-modifier 'none ;; default emacs
@@ -10,9 +63,15 @@
  mac-right-option-modifier 'none ;; emacs mac port
  mac-option-modifier 'meta
  ;; ns-use-native-fullscreen t
+ org-agenda-files '("~/Documents/org-agenda/")
  )
+;; enable pretty font rendering or something
+                                        ; (mac-auto-operator-composition-mode)
+                                        ; (setq doom-font (font-spec :family "Fira Code" :size 13))
 
 (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3") ;; fix package download issues on ELPA
+
+(setq! evil-escape-key-sequence "jj")
 
 (setq ns-use-native-fullscreen t)
 
@@ -23,15 +82,22 @@
 (add-hook! js2-mode 'prettier-js-mode)
 (add-hook! typescript-mode 'prettier-js-mode)
 
-;; (setq doom-theme 'doom-spacegrey)
+;;(setq doom-theme 'doom-gruvbox)
 
 (add-hook 'before-save-hook #'delete-trailing-whitespace)
 ;; (add-to-list 'auto-mode-alist (cons (purecopy "\\.vtc\\'") 'vcl-mode))
 
-(def-package! vcl-mode
+(use-package! vcl-mode
   :config
   (add-to-list 'auto-mode-alist (cons (purecopy "\\.vtc\\'") 'vcl-mode)))
 
-(def-package! yaml-mode
+(use-package! yaml-mode
   :config
   (add-to-list 'auto-mode-alist (cons (purecopy "\\.yml\\'") 'yaml-mode)))
+
+(defun toggle-fullscreen ()
+  "Toggle full screen"
+  (interactive)
+  (set-frame-parameter
+   nil 'fullscreen
+   (when (not (frame-parameter nil 'fullscreen)) 'fullboth)))
