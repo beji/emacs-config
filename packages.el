78e5b7a9ee809c6660d2a0788e5ab17506ee79e5
Björn Erlwein <bjoernerlwein@gmail.com>
2020-08-14 10:43:32 +0200

    latest

--- packages.el
+++ packages.el
@@ -31,7 +31,7 @@
 
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
-;; `:files' in the `:recipe':
+;; `:files' in the `:recipe:
 ;(package! this-package
 ;  :recipe (:host github :repo "username/repo"
 ;           :files ("some-file.el" "src/lisp/*.el")))
@@ -50,3 +50,5 @@
 ;; This is required for some packages whose default branch isn't 'master' (which
 ;; our package manager can't deal with; see raxod502/straight.el#279)
 ;(package! builtin-package :recipe (:branch "develop"))
+
+(package! zig-mode)
